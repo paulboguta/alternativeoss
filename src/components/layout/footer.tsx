92c1c4ad95a4e5319e881f7469c41a8e11b013d4
Pawel Boguta <paul.boguta@gmail.com>
2025-02-27 15:45:01 +0100

    add featured on startup fame link

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,12 +1,17 @@
 import { websiteConfig } from '@/config/website';
 import Link from 'next/link';
 
+const featuredOn = [
+  {
+    name: 'Startup Fame',
+    href: 'https://startupfa.me/s/alternativeoss?utm_source=alternativeoss.com',
+  },
+];
+
 export async function Footer() {
-  //   await connection();
-  //   const currentTime = Date.now();
   return (
-    <div className="container-wrapper border-t border-dashed p-8">
-      <div className="flex items-center justify-between">
+    <div className="container-wrapper space-y-4 border-y border-dashed">
+      <div className="flex items-center justify-between border-b px-8 py-4">
         <p className="text-muted-foreground text-sm">
           Built with 💚 by{' '}
           <Link
@@ -18,9 +23,20 @@ export async function Footer() {
             Pawel Boguta
           </Link>
         </p>
-        <p className="text-muted-foreground text-sm">
-          {/* &copy; {format(currentTime, 'yyyy')} {websiteConfig.name} */}
-        </p>
+        {/* badges / links where the project was featured */}
+        <div className="text-muted-foreground space-x-2 text-xs">
+          <span>Featured on</span>
+          {featuredOn.map(item => (
+            <a
+              key={item.name}
+              href={item.href}
+              target="_blank"
+              className="text-muted-foreground hover:underline"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
