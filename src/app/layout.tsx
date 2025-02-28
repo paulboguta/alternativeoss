b6dcabbfd72f0719d95b07040aca19e0e54cf68d
Pawel Boguta <paul.boguta@gmail.com>
2025-02-28 10:19:44 +0100

    migrate to plausible analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import Analytics from '@/components/analytics';
 import { Toaster } from '@/components/ui/sonner';
-import { metadata } from '@/config/website';
+import { metadata, websiteConfig } from '@/config/website';
 import { cn } from '@/lib/utils';
+import PlausibleProvider from 'next-plausible';
 import { Inter } from 'next/font/google';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import './globals.css';
@@ -12,18 +12,15 @@ export { metadata };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <Suspense>
-    // <ClerkProvider>
     <html lang="en">
       <body
         className={cn('bg-background dark min-h-screen font-sans antialiased', inter.className)}
       >
-        <Analytics />
         <Toaster />
-        <NuqsAdapter>{children}</NuqsAdapter>
+        <PlausibleProvider domain={websiteConfig.domain}>
+          <NuqsAdapter>{children}</NuqsAdapter>
+        </PlausibleProvider>
       </body>
     </html>
-    //   </ClerkProvider>
-    // </Suspense>
   );
 }
