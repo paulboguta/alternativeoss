aa602a0dcaadae0731ad052e6c5fec00bfe49bf9
Pawel Boguta <paul.boguta@gmail.com>
2025-03-01 12:12:38 +0100

    add metadata

--- src/app/(main)/alternatives/page.tsx
+++ src/app/(main)/alternatives/page.tsx
@@ -4,11 +4,59 @@ import { SkeletonAlternativesContent } from '@/components/alternative/skeleton-a
 import { Toolbar } from '@/components/toolbar/toolbar';
 import { ToolbarSkeleton } from '@/components/toolbar/toolbar-skeleton';
 import { ALTERNATIVES_SORT_OPTIONS, DEFAULT_SORT_ALTERNATIVES } from '@/config/sorting';
+import { websiteConfig } from '@/config/website';
 
 import { AlternativeSortField } from '@/types/sorting';
 
+import { Metadata } from 'next';
 import { SearchParams } from 'nuqs/server';
 import { Suspense } from 'react';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const title = 'Open Source Software Alternatives';
+  const description =
+    'Discover the best open source alternatives to popular software. Find free and open source alternatives for all your software needs.';
+
+  return {
+    title,
+    description,
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://alternativeoss.com/alternatives',
+      title,
+      description,
+      siteName: 'AlternativeOSS',
+      images: [
+        {
+          url: websiteConfig.links.ogImage,
+          width: 1200,
+          height: 630,
+          alt: 'Open Source Software Alternatives',
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [websiteConfig.links.ogImage],
+    },
+    alternates: {
+      canonical: 'https://alternativeoss.com/alternatives',
+    },
+    keywords: [
+      'open source alternatives',
+      'software alternatives',
+      'open source software',
+      'free software',
+      'OSS',
+      'FOSS',
+      'alternative software',
+      'free and open source alternatives',
+    ],
+  };
+}
 
 export default async function AlternativesPage({
   searchParams,
@@ -18,7 +66,7 @@ export default async function AlternativesPage({
   const awaitedSearchParams = await searchParams;
 
   return (
-    <div>
+    <>
       <AlternativesHeader />
 
       <Suspense fallback={<ToolbarSkeleton />}>
@@ -34,6 +82,6 @@ export default async function AlternativesPage({
           <AlternativesContent searchParams={awaitedSearchParams} />
         </Suspense>
       </section>
-    </div>
+    </>
   );
 }
